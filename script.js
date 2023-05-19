e55e774ede5b1e7be832514267c1b2bcb36803c0
DurgeshDM <133991660+DurgeshDM@users.noreply.github.com>
2023-05-19 11:27:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// var confirmation = confirm() ;
+
+// console.log(confirmation);
+// if (confirmation === true) {
+//     window.location = ("https://github.com/vishwajitvm") ;
+// }
+// else {
+//     window.location = ("abcccc") ;
+
+// }
